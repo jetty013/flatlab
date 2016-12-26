5007e737b941379813869a7a7470df02d5c1d9f1
jetty013 <1424981970@qq.com>
2016-12-26 09:41:28 +0800

    change1

---
+++ js/public.js
@@ -0,0 +1,8 @@
+//头部搜索框效果
+(function (){
+    var inputNO_1=document.getElementById("search");
+    inputNO_1.onclick=function(){
+        inputNO_1.className=="normal" ? inputNO_1.className="fixed" : inputNO_1.className="normal";
+    };
+})();
+//Portfolio页面选项卡图片筛选效果实现
